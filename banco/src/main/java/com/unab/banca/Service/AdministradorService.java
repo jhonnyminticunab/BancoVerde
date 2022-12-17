a4d228daffbb5cfce1f5cb2fd6111aa1bd99119a
Jorge Gutierrez <jorge.gutierrez1.mt@usa.edu.co>
2022-12-17 07:38:21 -0500

    Administrador 16-dic

---
+++ banco/src/main/java/com/unab/banca/Service/AdministradorService.java
@@ -0,0 +1,34 @@
+package com.unab.banca.Service;
+import com.unab.banca.Models.Administrador;
+import com.unab.banca.Dao.AdministradorDao;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Service; 
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AdministradorService {
+    @Autowired
+    private AdministradorDao administradorDao;
+
+    @Transactional(readOnly=false)
+    public Administrador save(AdministradorDao administradorDao2) {
+        return administradorDao.save(administradorDao);
+    }
+
+    @Transactional(readOnly=false)
+    public void delete(String id) {
+        administradorDao.deleteById(id);
+    }
+
+    @Transactional(readOnly=true)
+    public Administrador findById(String id) {
+        return administradorDao.findById(id).orElse(null);
+    }
+
+    @Transactional(readOnly=true)
+    public List<Administrador> findAll() {
+        return (List<Administrador>) administradorDao.findAll();
+    }
+
+}
